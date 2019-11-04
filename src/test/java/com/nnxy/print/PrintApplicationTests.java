f8391bf06c826b868e7a4d36ba824d8bbcb0472f
litianfu <1035869369@qq.com>
2019-11-04 18:24:45 +0800

    "初始化打印项目"

---
+++ src/test/java/com/nnxy/print/PrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnxy.print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
